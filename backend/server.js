d302a9f1e6a49b7abe9123d9572abb45e2621074
moedeera <82620264+moedeera@users.noreply.github.com>
2023-03-27 19:34:48 -0600

    committ

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
 console.log("hello");
+const express = require("express");
+const dotenv = require("dotenv").config;
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world?????");
+});
+
+app.listen(port, () => console.log(port));
