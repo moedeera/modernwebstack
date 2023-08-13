63dd529c85c1cadd5e182c25347d0952a0b002af
Moe Deera <deeramoe@gmail.com>
2023-08-12 22:18:00 -0600

    committ

--- frontend/src/Pages/Homepage/Homepage.jsx
+++ frontend/src/Pages/Homepage/Homepage.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./Homepage.css";
 
 export const Homepage = () => {
-  return <div className="container homepage">Homepage</div>;
+  return (
+    <div className="container homepage">
+      <div className="landing">
+        <div className="landing-text">
+          <h1>Holistic Approach to Web Design</h1>
+          <p></p>
+          <button></button>
+        </div>
+        <div className="landing-pictures"></div>
+      </div>
+    </div>
+  );
 };
