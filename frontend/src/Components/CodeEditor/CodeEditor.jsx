f8c58676cf7e671aaf2dba8baa4ee35762bd5644
moedeera <82620264+moedeera@users.noreply.github.com>
2023-11-28 16:11:48 -0600

    committ

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useCallback } from "react";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import "./CodeEditor.css";
+import { codeblock1 } from "../../Context/Content";
+const activateMonacoJSXHighlighter = async (monacoEditor, monaco) => {
+  const { default: traverse } = await import("@babel/traverse");
+  const { parse } = await import("@babel/parser");
+  const { default: MonacoJSXHighlighter } = await import(
+    "monaco-jsx-highlighter"
+  );
 
-const CodeEditor = ({ code, onChange }) => {
-  return <> </>;
+  const monacoJSXHighlighter = new MonacoJSXHighlighter(
+    monaco,
+    parse,
+    traverse,
+    monacoEditor
+  );
+
+  monacoJSXHighlighter.highlightOnDidChangeModelContent();
+  monacoJSXHighlighter.addJSXCommentCommand();
+
+  return {
+    monacoJSXHighlighter,
+  };
+};
+
+const CodeEditor = () => {
+  const handleEditorDidMount = useCallback(async (editor, monaco) => {
+    activateMonacoJSXHighlighter(editor, monaco);
+  }, []);
+  return (
+    <div className="code-editor">
+      <Editor
+        height="650px"
+        width="50%"
+        language="typescript"
+        value={codeblock1}
+        theme="vs-dark"
+        onMount={handleEditorDidMount}
+        options={{
+          selectOnLineNumbers: true,
+          // automaticLayout: true,
+          readOnly: true, // Set to true to disable editing
+        }}
+      />
+    </div>
+  );
 };
 
 export default CodeEditor;
--- frontend/src/Pages/Homepage/Homepage.jsx
+++ frontend/src/Pages/Homepage/Homepage.jsx
@@ -5,6 +5,7 @@ import { Block1 } from "../../Components/Block1/Block1";
 import { landingPageAbout, landingPageLearning } from "../../Context/Content";
 import { Block0 } from "../../Components/Block0/Block0";
 import { Block3 } from "../../Components/Block3/Block3";
+import CodeEditor from "../../Components/CodeEditor/CodeEditor";
 
 export const Homepage = () => {
   return (
@@ -13,7 +14,10 @@ export const Homepage = () => {
       <Block1 data={landingPageAbout} />
       <Block1 data={landingPageLearning} />
       <Block3 />
-      <Block0 />
+      {/* <Block0 /> */}
+
+      <CodeEditor />
+      <CodeEditor />
     </div>
   );
 };
