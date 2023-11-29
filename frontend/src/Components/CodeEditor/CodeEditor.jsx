ac8a69e29c78fa4bc71aada01795ec35a68a59e1
Moe Deera <deeramoe@gmail.com>
2023-11-29 01:59:27 -0600

    committ

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -2,32 +2,8 @@ import React, { useCallback } from "react";
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 import "./CodeEditor.css";
 import { codeblock1 } from "../../Context/Content";
-const activateMonacoJSXHighlighter = async (monacoEditor, monaco) => {
-  const { default: traverse } = await import("@babel/traverse");
-  const { parse } = await import("@babel/parser");
-  const { default: MonacoJSXHighlighter } = await import(
-    "monaco-jsx-highlighter"
-  );
-
-  const monacoJSXHighlighter = new MonacoJSXHighlighter(
-    monaco,
-    parse,
-    traverse,
-    monacoEditor
-  );
-
-  monacoJSXHighlighter.highlightOnDidChangeModelContent();
-  monacoJSXHighlighter.addJSXCommentCommand();
-
-  return {
-    monacoJSXHighlighter,
-  };
-};
 
 const CodeEditor = () => {
-  const handleEditorDidMount = useCallback(async (editor, monaco) => {
-    activateMonacoJSXHighlighter(editor, monaco);
-  }, []);
   return (
     <div className="code-editor">
       <Editor
@@ -36,7 +12,6 @@ const CodeEditor = () => {
         language="typescript"
         value={codeblock1}
         theme="vs-dark"
-        onMount={handleEditorDidMount}
         options={{
           selectOnLineNumbers: true,
           // automaticLayout: true,
