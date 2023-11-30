be057136f2290040106cc678ba88e4dd62c6e68f
Moe Deera <deeramoe@gmail.com>
2023-11-30 03:44:44 -0600

    committ

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -1,25 +1,8 @@
 import React, { useCallback } from "react";
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 import "./CodeEditor.css";
-import { codeblock1, codeblock2 } from "../../Context/Content";
 
 const CodeEditor = () => {
-  return (
-    <div className="code-editor">
-      <Editor
-        height="650px"
-        width="50%"
-        language="typescript"
-        value={codeblock2}
-        theme="vs-dark"
-        options={{
-          selectOnLineNumbers: false,
-          // automaticLayout: true,
-          readOnly: true, // Set to true to disable editing
-        }}
-      />
-    </div>
-  );
+  return <div className="code-editor"></div>;
 };
 
 export default CodeEditor;
