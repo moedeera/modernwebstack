252c34739cac0f91589e26363af948c91477f358
Moe Deera <deeramoe@gmail.com>
2023-12-03 06:15:04 -0600

    committ

--- frontend/src/Components/Block4/Block4.jsx
+++ frontend/src/Components/Block4/Block4.jsx
@@ -3,22 +3,25 @@ import "./Block4.css";
 
 export const Block4 = () => {
   return (
-    <div className="block-4-container">
-      <div className="upper-dots">
-        <div className="b4-dot"></div>
-        <div className="b4-dot"></div>
-        <div className="b4-dot"></div>
+    <div className="block-4-box">
+      {" "}
+      <div className="block-4-container">
+        <div className="upper-dots">
+          <div className="b4-dot"></div>
+          <div className="b4-dot"></div>
+          <div className="b4-dot"></div>
+        </div>
+        <h3>Stunning Templates </h3>
+        <p>
+          An amazing customizable block that has it's own CSS styling ready to
+          go so you don't have to worry about setting up everything yourself.
+          Copy and Paste and viola ! you have a professional sleek looking
+          component just lie that.
+        </p>
+        <Link to={"/"} className="btn">
+          Learn More
+        </Link>
       </div>
-      <h3>Check out this Block</h3>
-      <p>
-        An amazing customizable block that has it's own CSS styling ready to go
-        so you don't have to worry about setting up everything yourself. Copy
-        and Paste and viola ! you have a professional sleek looking component
-        just lie that.
-      </p>
-      <Link to={"/"} className="btn">
-        Learn More
-      </Link>
     </div>
   );
 };
