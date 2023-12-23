2635738d07960a54d5ea776d0f0307af26d413a2
Moe Deera <deeramoe@gmail.com>
2023-12-23 05:37:32 -0600

    committ

--- frontend/src/Components/SubBlock1/SubBlock1.jsx
+++ frontend/src/Components/SubBlock1/SubBlock1.jsx
@@ -1,11 +1,46 @@
 import "./SubBlock1.css";
 
+import gear from "./gear.png";
+import down from "./down.png";
+
 export const SubBlock1 = () => {
   return (
     <div className="sub-block-1-container">
-      <div className="sub-block-1-jsx"></div>
-      <div className="sub-block-1-css"></div>
-      <div className="sub-block-1-files"></div>
+      <div className="sub-block-1-jsx">
+        <div className="sb1-upper-portion">
+          <div className="sb1-label">
+            <img src={gear} alt="" srcset="" />
+            JSX
+          </div>
+          <div className="sb1-icon">
+            <img src={down} alt="" srcset="" />
+          </div>
+        </div>
+      </div>
+      <div className="sub-block-1-css">
+        {" "}
+        <div className="sb1-upper-portion">
+          <div className="sb1-label">
+            <img src={gear} alt="" srcset="" />
+            CSS
+          </div>
+          <div className="sb1-icon">
+            <img src={down} alt="" srcset="" />
+          </div>
+        </div>
+      </div>
+      <div className="sub-block-1-files">
+        {" "}
+        <div className="sb1-upper-portion">
+          <div className="sb1-label">
+            <img src={gear} alt="" srcset="" />
+            JS
+          </div>
+          <div className="sb1-icon">
+            <img src={down} alt="" srcset="" />
+          </div>
+        </div>
+      </div>
       <div className="sub-block-1-image"></div>
     </div>
   );
