b7170020db9d2db65c254a28987bea1ff165721f
Moe Deera <deeramoe@gmail.com>
2024-01-17 02:32:12 -0600

    committ

--- frontend/src/Pages/Docs/Docs.jsx
+++ frontend/src/Pages/Docs/Docs.jsx
@@ -1,3 +1,4 @@
+import { CodeBlock } from "../../Components/CodeBlock/CodeBlock";
 import "./Docs.css";
 
 export const Docs = () => {
@@ -63,7 +64,38 @@ export const Docs = () => {
           </div>
         ))}
       </div>
-      <div className="docs-content-container"></div>
+      <div className="docs-content-container">
+        <h3>Getting Started</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <div className="docs-content-codeblock">
+          <CodeBlock />
+        </div>
+      </div>
     </div>
   );
 };
