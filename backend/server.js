64091c0be9730d1107272e9d20f8279f59125674
Moe Deera <deeramoe@gmail.com>
2024-02-04 05:57:53 -0600

    committ

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,3 @@
-// console.log("hello");
-
-// const express = require("express");
-// const dotenv = require("dotenv").config;
-// const port = process.env.PORT || 5000;
-// const path = require("path");
-// const cors = require("cors");
-// const app = express();
-
-// app.use("/api/testing", require("./Routes/TestRoutes"));
-
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, "../", "frontend", "dist", "index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) =>
-//     res.send(
-//       `Please set to production, you are currently in ${process.env.NODE_ENV}`
-//     )
-//   );
-// }
-
-// app.listen(port, () => console.log(port));
 const express = require("express");
 const dotenv = require("dotenv").config(); // Ensure dotenv is called as a function
 const port = process.env.PORT || 5000;
@@ -66,3 +32,39 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+//Old code
+// console.log("hello");
+
+// const express = require("express");
+// const dotenv = require("dotenv").config;
+// const port = process.env.PORT || 5000;
+// const path = require("path");
+// const cors = require("cors");
+// const app = express();
+
+// app.use("/api/testing", require("./Routes/TestRoutes"));
+
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) =>
+//     res.send(
+//       `Please set to production, you are currently in ${process.env.NODE_ENV}`
+//     )
+//   );
+// }
+
+// app.listen(port, () => console.log(port));
