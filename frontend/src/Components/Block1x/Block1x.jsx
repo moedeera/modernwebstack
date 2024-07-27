09f4b1f87e591be2b854b2968d3aa931e692ca67
Moe Deera <deeramoe@gmail.com>
2024-07-27 05:34:09 -0600

    committ

---
+++ frontend/src/Components/Block1x/Block1x.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./Block1x.css";
+import { Link } from "react-router-dom";
+
+import { SubBlock2 } from "../SubBlock2/SubBlock2";
+
+export const Block1x = ({ data }) => {
+  const [info, setInfo] = useState({
+    small: "Fundamentals",
+    header: "You need to understand design to know what you want",
+    text: ` UI is a simple yet complex concept for most developers. Understanding what looks good is easy. Breaking down the concepts to build
+    good looking component is hard.
+    `,
+    text_sec: `Thats why we simplify things for you. with a breakdown of these concepts.`,
+    buttonText: "Learn More",
+    link: "/",
+    reverse: true,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setInfo(data);
+    }
+  }, []);
+  return (
+    <div
+      className={
+        info.reverse ? "Block-1x-container reverse" : "Block-1x-container"
+      }
+    >
+      <div className="Block-1x-text">
+        <small>{info.small}</small>
+        <h3>{info.header}</h3>
+        <p>{info.text}</p>
+        {/* <br /> */}
+        <p>{info.text_sec}</p>
+        <Link className="btn btn-primary" to={info.link}>
+          {info.buttonText}
+        </Link>
+      </div>
+      <div className="Block-1x-image">
+        {/* <img src={basicImage} alt="" /> */}
+        <SubBlock2 />
+      </div>
+    </div>
+  );
+};
--- frontend/src/Pages/Template/Template.jsx
+++ frontend/src/Pages/Template/Template.jsx
@@ -1,3 +1,5 @@
+import { Block0 } from "../../Components/Block0/Block0";
+import { Block1x } from "../../Components/Block1x/Block1x";
 import { Block3 } from "../../Components/Block3/Block3";
 import { CodeBlock } from "../../Components/CodeBlock/CodeBlock";
 
@@ -9,11 +11,10 @@ export const Template = () => {
   return (
     <div className="container homepage">
       <TemplatePageLanding />
-      <Block3 />
+      <Block1x />
       <div className="main-code-block">
         <CodeBlock />
       </div>
-      <SubBlock0 />
     </div>
   );
 };
