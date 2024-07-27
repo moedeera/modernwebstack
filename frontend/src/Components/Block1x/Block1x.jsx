6d466ee2eaf920244b0c430748e8552a038dc656
Moe Deera <deeramoe@gmail.com>
2024-07-27 06:02:21 -0600

    committ

--- frontend/src/Components/Block1x/Block1x.jsx
+++ frontend/src/Components/Block1x/Block1x.jsx
@@ -13,7 +13,7 @@ export const Block1x = ({ data }) => {
     good looking component is hard.
     `,
     text_sec: `Thats why we simplify things for you. with a breakdown of these concepts.`,
-    buttonText: "Learn More",
+    buttonText: "Documentation",
     link: "/",
     reverse: false,
   });
@@ -31,13 +31,25 @@ export const Block1x = ({ data }) => {
     >
       <div className="Block-1x-text">
         <small>{info.small}</small>
-        <h3>{info.header}</h3>
+        <h3>
+          {info.header.substring(0, 12)}
+          <span>{info.header.substring(12, 29)}</span>
+          {info.header.substring(29, 54)}
+        </h3>
         <p>{info.text}</p>
         {/* <br /> */}
-        <p>{info.text_sec}</p>
-        <Link className="btn btn-primary" to={info.link}>
-          {info.buttonText}
-        </Link>
+        <p>
+          {info.text_sec} <span></span>
+        </p>
+        <div className="btn-container">
+          {" "}
+          <Link className="btn btn-primary" to={info.link}>
+            {info.buttonText}
+          </Link>{" "}
+          <Link className="btn btn-secondary" to={info.link}>
+            See Templates
+          </Link>{" "}
+        </div>
       </div>
       <div className="Block-1x-image">
         {/* <img src={basicImage} alt="" /> */}
