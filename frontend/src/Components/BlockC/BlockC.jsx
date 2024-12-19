a2646c8c6e3e6a2e3b9d859cbdef656c2f2acb64
moedeera <82620264+moedeera@users.noreply.github.com>
2024-12-19 15:25:18 -0600

    block c setup
    block c setup but there is still a bug causing the last box to abruptly expand vertically.

--- frontend/src/Components/BlockC/BlockC.jsx
+++ frontend/src/Components/BlockC/BlockC.jsx
@@ -2,17 +2,42 @@ import React, { useState } from "react";
 import "./BlockC.css";
 
 const BlockC = () => {
-  const [view, setView] = useState(0);
+  const [currentView, setCurrentView] = useState(0);
+  const views = [
+    { id: 1, name: "Simple", viewNumber: 0 },
+    { id: 2, name: "Semi-Functional", viewNumber: 1 },
+    { id: 3, name: "Full Functional", viewNumber: 2 },
+    { id: 4, name: "Layouts", viewNumber: 3 },
+  ];
   return (
     <div>
       {" "}
       <div className="block-c-container">
         {" "}
-        <h2>Filter based on your needs</h2>
+        <div>
+          <h2>Filter based on your needs</h2>
+          <div className="block-c-buttons">
+            {views.map((view) => (
+              <div
+                key={view.id}
+                className={
+                  currentView === view.viewNumber
+                    ? "btn btn-alt block-c-selected"
+                    : "btn btn-alt"
+                }
+                onClick={() => {
+                  setCurrentView(view.viewNumber);
+                }}
+              >
+                {view.name}
+              </div>
+            ))}
+          </div>
+        </div>
         <div className="block-c-scrolling-container">
           <div
             className="block-c-scrolling-contents"
-            style={{ transform: `translateY(-${view * 25}%)` }}
+            style={{ transform: `translateX(-${currentView * 100}%)` }}
           >
             {" "}
             <div className="block-c-box" id="box-1">
@@ -27,7 +52,7 @@ const BlockC = () => {
               {" "}
               C{" "}
             </div>
-            <div className="block-c-box " id="box-4">
+            <div className="block-c-box" id="box-4">
               {" "}
               D
             </div>
